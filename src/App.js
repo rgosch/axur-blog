2c1a3cdc1f4f5841b29da9bce605143411133e1e
Gosch <pro@gosch.io>
2020-04-19 21:26:46 -0300

    Implements the eslint, prettier and githooks

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const App = () => (
-  <>Axur Blog</>
-);
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const App = () => {
+  return <>Axur Blog</>;
+};
+
+export default App;
